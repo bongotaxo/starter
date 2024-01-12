1ba42a02e77a9586e2f95e49f36be6fa6bae5dbe
Hopheylalal <53647039+Hopheylalal@users.noreply.github.com>
2024-01-12 13:06:56 +0300

    1

--- lib/main.dart
+++ lib/main.dart
@@ -4,18 +4,21 @@ import 'package:dart_appwrite/dart_appwrite.dart';
 // This is your Appwrite function
 // It's executed each time we get a request
 Future<dynamic> main(final context) async {
-// Why not try the Appwrite SDK?
-  //
-  // final client = Client()
-  //    .setEndpoint('https://cloud.appwrite.io/v1')
-  //    .setProject(Platform.environment['APPWRITE_FUNCTION_PROJECT_ID'])
-  //    .setKey(Platform.environment['APPWRITE_API_KEY']);
+  final client = Client()
+      .setEndpoint('https://cloud.appwrite.io/v1')
+      .setProject('bongotakso')
+      .setKey(
+          '1c822e7306250836f8cc5e7a533d68afeaeb50807e01f2e21abc9c70d03c3306027c7382bfc0716e9de4e1168f54a9bcfaf9a213c644ec4227ef7113275272a3ede51c1508459c24ba0939e23bed35a93447a8155c927851e22ab5b819dd9e886bca52b17848c8c59d80ed86958358858fd8986b6cec8d04fede28c0c4ce3fdd');
 
   // You can log messages to the console
-  context.log('Hello, Logs HIIIIIII!');
+  context.log('Hello, Logs HIIIIIII000!');
+  context.log(context.req.body());
+
 
   // If something goes wrong, log an error
   context.error('Hello, Errors!');
+
+  print(context.req);
 
   // The `req` object contains the request data
   if (context.req.method == 'GET') {
